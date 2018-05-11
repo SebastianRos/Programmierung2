6832e2bcfbf8598ce950a11a316531b9dcbcb3c7
SebastianRos <sebastian-rosauer@outlook.de>
2018-05-11 19:46:45 +0200

    Ab Nr. 2 beendet

--- ab03_A2_IteratorFuerArray2/IteratorFuerArray2.java
+++ ab03_A2_IteratorFuerArray2/IteratorFuerArray2.java
@@ -1,21 +1,21 @@
 /**
  * @author srasau2s
  */
-package ab03_A2_IteratorFuerArray2;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class IteratorFuerArray2<T> implements Iterator<T>{
 	
-	T[][] a;
-	boolean hasNext = true;
-	boolean istZeilenweise = true;
-	int richtung = 0;
-	int zStart = -1;
-	int sStart = -1;
-	int posZ = -1;
-	int posS = -1;
-	
+	private T[][] a;
+	private boolean hasNext = true;
+	private boolean istZeilenweise = true;
+	private int richtung = 0;
+	private int zStart = -1;
+	private int sStart = -1;
+	private int posZ = -1;
+	private int posS = -1;
+
 	public IteratorFuerArray2(T[][] pA) {
 		this(pA, true);
 		a = pA;
@@ -46,37 +46,43 @@ public class IteratorFuerArray2<T> implements Iterator<T>{
 		return hasNext;
 	}
 	public T next() {
-		T retval = a[posZ][posS];
-		if (istZeilenweise) {
-			posS = (posS + richtung);
-			if (posS >= a.length) {
-				posS = 0;
-				posZ++;
-			} else if (posS < 0) {
-				posS = a.length-1;
-				posZ--;
-			}
-			if (posZ >= a.length) {
-				posZ = 0;
-			} else if (posZ < 0) {
-				posZ = a.length-1;
-			}			
-		} else {
-			posZ = (posZ + richtung);
-			if (posZ >= a.length) {
-				posZ = 0;
-				posS++;
-			} else if (posZ < 0) {
-				posZ = a.length-1;
-				posS--;
-			}
-			if (posS >= a.length) {
-				posS = 0;
-			} else if (posS < 0) {
-				posS = a.length-1;
-			}
+		if (!hasNext()){
+			throw new NoSuchElementException();
 		}
+		T retval = a[posZ][posS];
+		if (istZeilenweise){
+			posS += richtung;
+		} else {
+			posZ += richtung;
+		}
+		clamp();
 		hasNext = ((posS == sStart) && (posZ == zStart)) ? false : true;
 		return retval;
+	}
+
+	private void clamp(){
+		if ((posS + posZ) >= (a[0].length + a.length-1)){
+			posS = 0;
+			posZ = 0;
+		} else if (posS + posZ <= -1){
+			posS = a[0].length-1;
+			posZ = a.length-1;
+		}
+		if (posZ >= a.length){
+			posZ = 0;
+			posS++;
+		} else if (posZ < 0){
+			posZ = a.length-1;
+			posS--;
+		}
+		if (posS >= a[0].length){
+			posS = 0;
+			posZ++;
+			clamp();
+		} else if (posS < 0){
+			posS = a[0].length-1;
+			posZ--;
+			clamp();
+		}
 	}
 }
